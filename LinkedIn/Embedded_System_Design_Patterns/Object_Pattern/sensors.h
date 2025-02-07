1e271b37b55e294af96619f3bf77ec901801fb12
Yashwanth Naidu Tikkisetty <114215290+T-Yashwanth-Naidu@users.noreply.github.com>
2025-02-07 00:56:48 -0500

    Update sensors.h

--- LinkedIn/Embedded_System_Design_Patterns/Object_Pattern/sensors.h
+++ LinkedIn/Embedded_System_Design_Patterns/Object_Pattern/sensors.h
@@ -3,24 +3,66 @@
 
 #include <stdint.h>
 
-// Ultrasonic Sensor for obstacle detection.
+/**
+ * @brief Structure representing an ultrasonic sensor used for obstacle detection.
+ *
+ * This sensor measures the distance between the vehicle and an obstacle.
+ * The distance value is used by the AEB system to determine whether braking is necessary.
+ */
 struct UltrasonicSensor {
-    float distance;
+    float distance; /**< Distance reading from the sensor in meters. */
 };
 
-// Wheel Speed Sensor for measuring vehicle speed.
+/**
+ * @brief Structure representing a wheel speed sensor.
+ *
+ * This sensor measures the current speed of the vehicle.
+ * The speed value is used to assess whether the vehicle is in motion and to adjust dynamic behavior.
+ */
 struct SpeedSensor {
-    float speed;
+    float speed; /**< Current speed reading from the sensor in meters per second. */
 };
 
-// Brake Pressure Sensor.
+/**
+ * @brief Structure representing a brake pressure sensor.
+ *
+ * This sensor monitors the pressure applied to the brakes.
+ * The pressure reading helps determine the state of the braking system.
+ */
 struct BrakePressureSensor {
-    float pressure;
+    float pressure; /**< Current brake pressure reading in PSI (pounds per square inch). */
 };
 
-// Function prototypes for sensor updates.
+/* Function Prototypes for Sensor Updates */
+
+/**
+ * @brief Updates the distance reading of an ultrasonic sensor.
+ *
+ * This function sets the distance value of the given UltrasonicSensor object.
+ *
+ * @param self Pointer to an UltrasonicSensor instance.
+ * @param distance New distance value to be updated (in meters).
+ */
 void ultrasonic_sensor_update(struct UltrasonicSensor *self, float distance);
+
+/**
+ * @brief Updates the speed reading of a wheel speed sensor.
+ *
+ * This function sets the speed value of the given SpeedSensor object.
+ *
+ * @param self Pointer to a SpeedSensor instance.
+ * @param speed New speed value to be updated (in meters per second).
+ */
 void speed_sensor_update(struct SpeedSensor *self, float speed);
+
+/**
+ * @brief Updates the brake pressure reading of a brake pressure sensor.
+ *
+ * This function sets the pressure value of the given BrakePressureSensor object.
+ *
+ * @param self Pointer to a BrakePressureSensor instance.
+ * @param pressure New pressure value to be updated (in PSI).
+ */
 void brake_pressure_sensor_update(struct BrakePressureSensor *self, float pressure);
 
-#endif
+#endif /* SENSORS_H */
